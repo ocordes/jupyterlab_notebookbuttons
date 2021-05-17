0adc9a88f11997696cd170a38cfc7c482ace96c2
Oliver Cordes <ocordes@astro.uni-bonn.de>
2021-05-17 10:54:25 +0200

    - first working version - going to release 0.1.0

--- src/index.ts
+++ src/index.ts
@@ -3,85 +3,147 @@ import {
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
 
-import {
-  IDisposable, DisposableDelegate
-} from '@lumino/disposable';
+import { IDisposable, DisposableDelegate } from '@lumino/disposable';
 
+import { ToolbarButton } from '@jupyterlab/apputils';
 
-import {
-  ToolbarButton
-} from '@jupyterlab/apputils';
+import { DocumentRegistry } from '@jupyterlab/docregistry';
 
 import {
-  DocumentRegistry
-} from '@jupyterlab/docregistry';
-
-import {
-  NotebookActions, NotebookPanel, INotebookModel
+  NotebookActions,
+  NotebookPanel,
+  INotebookModel
 } from '@jupyterlab/notebook';
 
-
+import { ISettingRegistry } from '@jupyterlab/settingregistry';
+import { toArray } from '@lumino/algorithm';
 
 /**
  * A notebook widget extension that adds a button to the toolbar.
  */
-export
-  class ButtonExtension implements DocumentRegistry.IWidgetExtension<NotebookPanel, INotebookModel> {
+export class ButtonExtension
+  implements DocumentRegistry.IWidgetExtension<NotebookPanel, INotebookModel>
+{
+  settings: ISettingRegistry.ISettings;
+
+  constructor(protected setting_registry: ISettingRegistry) {
+    console.log('constructor');
+    // read the settings
+    this.setup_settings();
+  }
+
+  setup_settings(): void {
+    Promise.all([this.setting_registry.load(extension.id)])
+      .then(([settings]) => {
+        console.log('reading settings');
+        this.settings = settings;
+        // update of settings is done automatically
+        //settings.changed.connect(() => {
+        //  this.update_settings(settings);
+        //});
+      })
+      .catch((reason: Error) => {
+        console.error(reason.message);
+      });
+  }
+
   /**
    * Create a new extension object.
    */
-  createNew(panel: NotebookPanel, context: DocumentRegistry.IContext<INotebookModel>): IDisposable {
-    let callback_clearall = () => {
+  createNew(
+    panel: NotebookPanel,
+    context: DocumentRegistry.IContext<INotebookModel>
+  ): IDisposable {
+    // read all settings
+    const have_runall = this.settings.get('runall').composite;
+    const have_clearalloutputs = this.settings.get('clearalloutputs').composite;
+    const have_deletecells = this.settings.get('deletecells').composite;
+    let insert_before = this.settings.get('insertbefore').composite.toString();
+
+    // sanity check, if insert_before doesn't exist use the first element ...
+    const names = toArray(panel.toolbar.names());
+    if (names.includes(insert_before) === false) {
+      insert_before = names[0];
+    }
+
+    const callback_clearall = () => {
       NotebookActions.clearAllOutputs(panel.content);
     };
-    let callback_deletecells = () => {
+    const callback_deletecells = () => {
       NotebookActions.deleteCells(panel.content);
-    }
-    let callback_runall = () => {
+    };
+    const callback_runall = () => {
       NotebookActions.runAll(panel.content, context.sessionContext);
     };
-    let clearall_button = new ToolbarButton({
+    const clearall_button = new ToolbarButton({
       className: 'jp-nb-buttons',
       iconClass: 'fa fa-snowplow',
       onClick: callback_clearall,
       tooltip: 'Clear All Outputs'
     });
 
-    let deletecells_button = new ToolbarButton({
+    const deletecells_button = new ToolbarButton({
       className: 'jp-nb-buttons',
       iconClass: 'fa fa-trash-alt',
       onClick: callback_deletecells,
       tooltip: 'Delete Cells'
     });
 
-    let runall_button = new ToolbarButton({
+    const runall_button = new ToolbarButton({
       className: 'jp-nb-buttons',
       iconClass: 'fa fa-fast-forward',
       onClick: callback_runall,
       tooltip: 'Run all'
-    })
-  
+    });
+
     // start column to append the icons
-    let i = 10;
-    panel.toolbar.insertItem(i++, 'jp-nb-deletecells', deletecells_button);
-    panel.toolbar.insertItem(i++, 'jp-nb-runall', runall_button);
-    panel.toolbar.insertItem(i++, 'jp-nb-cleaarall', clearall_button);
+    if (have_deletecells === true) {
+      panel.toolbar.insertBefore(
+        insert_before,
+        'jp-nb-deletecells',
+        deletecells_button
+      );
+    }
+    if (have_runall === true) {
+      panel.toolbar.insertBefore(insert_before, 'jp-nb-runall', runall_button);
+    }
+    if (have_clearalloutputs === true) {
+      panel.toolbar.insertBefore(
+        insert_before,
+        'jp-nb-cleaarall',
+        clearall_button
+      );
+    }
+
     return new DisposableDelegate(() => {
-      runall_button.dispose();
-      clearall_button.dispose();
+      if (have_deletecells === true) {
+        deletecells_button.dispose();
+      }
+      if (have_runall === true) {
+        runall_button.dispose();
+      }
+      if (have_clearalloutputs === true) {
+        clearall_button.dispose();
+      }
     });
   }
 }
 
-
 /**
  * Activate the extension.
  */
-function activate(app: JupyterFrontEnd) {
-  app.docRegistry.addWidgetExtension('Notebook', new ButtonExtension());
-  console.debug('JupyterLab extension jupyterlab_notebookbuttons is activated!')
-};
-
+function activate(
+  app: JupyterFrontEnd,
+  setting_registry: ISettingRegistry
+): void {
+  app.docRegistry.addWidgetExtension(
+    'Notebook',
+    new ButtonExtension(setting_registry)
+  );
+  console.debug(
+    'JupyterLab extension jupyterlab_notebookbuttons is activated!'
+  );
+}
 
 /**
  * Initialization data for the jupyterlab_nbgrader extension.
@@ -89,6 +151,7 @@ function activate(app: JupyterFrontEnd) {
 const extension: JupyterFrontEndPlugin<void> = {
   id: 'jupyterlab_notebookbuttons:plugin',
   autoStart: true,
+  requires: [ISettingRegistry],
   activate: activate
 };
 
